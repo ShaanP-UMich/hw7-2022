fce175098b164e912b6fa5c41f760cb4b1ee63e3
Shaan Parikh <shaanp@umich.edu>
2022-10-27 17:59:45 -0400

    Making some progress

--- js/video.js
+++ js/video.js
@@ -1,22 +1,46 @@
-var video = document.getElementById('player1');
+// var video = document.getElementById('player1');
 
 window.addEventListener("load", function () {
 	console.log("Good job opening the window");
 
-	video.autoplay = false;
-	video.loop = false;
+	video = document.getElementById('player1');
 
-	console.log("Auto play is set to " + video.autoplay);
-	console.log("Loop is set to " + video.loop);
+	video.setAttribute('autoplay', false);
+	video.setAttribute('loop', false);
 
+	console.log("Auto play is set to " + video.getAttribute('autoplay'));
+	console.log("Loop is set to " + video.getAttribute('loop'));
+
+	addEventListeners();
 });
 
-// document.querySelector("#play").addEventListener("click", function () {
-// 	if (video) {
-// 		if (video.paused) {
-// 			video.play();
-// 		}
-// 	}
-// });
+function addEventListeners() {
+	document.querySelector("#play").addEventListener("click", () => {
+		if (video) {
+			if (video.paused) {
+				video.play();
+				updateVolumeInfo();
+			}
+		}
+	});
 
+	document.querySelector("#pause").addEventListener("click", () => {
+		if (video) {
+			if (!video.paused) {
+				video.pause();
+			}
+		}
+	});
 
+	document.querySelector("#slower").addEventListener("click", slowDown);
+}
+
+function updateVolumeInfo() {
+
+}
+
+function slowDown() {
+	let currVideoSpeed = video.getAttribute('playbackRate');
+
+	console.log(currVideoSpeed);
+}
